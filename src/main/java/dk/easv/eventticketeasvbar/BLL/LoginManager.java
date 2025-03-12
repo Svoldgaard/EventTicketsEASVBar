4a1b282217b6528687c528a0394ccb5e62046c89
Svoldgaard <Svoldgaard96@gmail.com>
2025-03-12 09:36:15 +0100

    made so LoginManager and LoginModel is ready to send the info to controller when it will be made after the Database

--- src/main/java/dk/easv/eventticketeasvbar/BLL/LoginManager.java
+++ src/main/java/dk/easv/eventticketeasvbar/BLL/LoginManager.java
@@ -1,4 +1,44 @@
 package dk.easv.eventticketeasvbar.BLL;
 
+import dk.easv.eventticketeasvbar.BE.Login;
+import dk.easv.eventticketeasvbar.DLL.Database.LoginDAO_DB;
+import dk.easv.eventticketeasvbar.DLL.DBConnection.DBConnection;
+
+import java.io.IOException;
+import java.util.List;
+
 public class LoginManager {
+    private final LoginDAO_DB loginDAO;
+
+    public LoginManager() throws IOException {
+        this.loginDAO = new LoginDAO_DB(new DBConnection()); // Inject DB connection
+    }
+
+
+    public boolean authenticate(String username, String password) throws Exception {
+        return loginDAO.verifyLogin(username, password);
+    }
+
+
+    public List<Login> getAllLogins() throws Exception {
+        return loginDAO.getAllLogin();
+    }
+
+
+    public void createLogin(String username, String password) throws Exception {
+        Login newLogin = new Login(username, password);
+        loginDAO.createLogin(newLogin);
+    }
+
+
+    public void updateLogin(String username, String newPassword) throws Exception {
+        Login updatedLogin = new Login(username, newPassword);
+        loginDAO.updateLogin(updatedLogin);
+    }
+
+
+    public void deleteLogin(String username) throws Exception {
+        Login loginToDelete = new Login(username, null);
+        loginDAO.deleteLogin(loginToDelete);
+    }
 }
--- src/main/java/dk/easv/eventticketeasvbar/GUI/Model/LoginModel.java
+++ src/main/java/dk/easv/eventticketeasvbar/GUI/Model/LoginModel.java
@@ -1,4 +1,40 @@
 package dk.easv.eventticketeasvbar.GUI.Model;
 
+import dk.easv.eventticketeasvbar.BLL.LoginManager;
+import dk.easv.eventticketeasvbar.BE.Login;
+
+import java.io.IOException;
+import java.util.List;
+
 public class LoginModel {
+    private final LoginManager loginManager;
+
+    public LoginModel() throws IOException {
+        this.loginManager = new LoginManager();
+    }
+
+
+    public boolean login(String username, String password) throws Exception {
+        return loginManager.authenticate(username, password);
+    }
+
+
+    public List<Login> getAllLogins() throws Exception {
+        return loginManager.getAllLogins();
+    }
+
+
+    public void createLogin(String username, String password) throws Exception {
+        loginManager.createLogin(username, password);
+    }
+
+
+    public void updateLogin(String username, String newPassword) throws Exception {
+        loginManager.updateLogin(username, newPassword);
+    }
+
+
+    public void deleteLogin(String username) throws Exception {
+        loginManager.deleteLogin(username);
+    }
 }
